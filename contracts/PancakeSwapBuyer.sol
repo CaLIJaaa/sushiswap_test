bdad46d954afb04be9e9136a2b9a0510fa7ba50c
CaLIJaaa <000lobovsasha000@mail.ru>
2025-06-28 00:25:05 +0300

    refactored

--- contracts/PancakeSwapBuyer.sol
+++ contracts/PancakeSwapBuyer.sol
@@ -1,40 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-interface IERC20 {
-    function transfer(address to, uint256 amount) external returns (bool);
-    function approve(address spender, uint256 amount) external returns (bool);
-    function balanceOf(address account) external view returns (uint256);
-}
-
-interface IWETH {
-    function deposit() external payable;
-    function transfer(address to, uint value) external returns (bool);
-}
-
-interface IPancakeSwapV2Factory {
-    function getPair(address tokenA, address tokenB) external view returns (address pair);
-}
-
-interface IPancakeSwapV2Pair {
-    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
-    function token0() external view returns (address);
-    function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external;
-}
-
-interface IPancakeSwapV2Router {
-    function addLiquidityETH(
-        address token,
-        uint amountTokenDesired,
-        uint amountTokenMin,
-        uint amountETHMin,
-        address to,
-        uint deadline
-    ) external payable returns (uint amountToken, uint amountETH, uint liquidity);
-}
+import "./interfaces/IERC20.sol";
+import "./interfaces/IWETH.sol";
+import "./interfaces/IPancakeSwapV2Factory.sol";
+import "./interfaces/IPancakeSwapV2Pair.sol";
+import "./interfaces/IPancakeSwapV2Router.sol";
 
 contract PancakeSwapBuyer {
-
+    
+    // --- if you want to deploy and use in on BSC, you need to change these constants ---
     address private constant PANCAKE_FACTORY = 0x1097053Fd2ea711dad45caCcc45EfF7548fCB362;
     address private constant PANCAKE_ROUTER = 0xEfF92A263d31888d860bD50809A8D171709b7b1c;
     address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
@@ -46,7 +21,6 @@ contract PancakeSwapBuyer {
     error InvalidToken();
     error InvalidAmount();
     error ApprovalFailed();
-    error DeadlineExpired();
 
     event TokensPurchased(address indexed token, uint256 amountOut, uint256 ethSpent);
     event LiquidityAdded(address indexed token, uint256 amountToken, uint256 amountETH, uint256 liquidity);
@@ -55,32 +29,10 @@ contract PancakeSwapBuyer {
         if (token == address(0) || token == WETH) revert InvalidToken();
         if (amountOut == 0) revert InvalidAmount();
 
-        address pair = IPancakeSwapV2Factory(PANCAKE_FACTORY).getPair(WETH, token);
-        if (pair == address(0)) revert PairNotFound();
-
-        (uint112 reserve0, uint112 reserve1,) = IPancakeSwapV2Pair(pair).getReserves();
-        if (reserve0 == 0 || reserve1 == 0) revert InsufficientLiquidity();
-        
-        address token0 = IPancakeSwapV2Pair(pair).token0();
-        bool isWETHToken0 = token0 == WETH;
-        
-        uint256 amountIn = _getAmountIn(
-            amountOut,
-            isWETHToken0 ? reserve0 : reserve1,
-            isWETHToken0 ? reserve1 : reserve0
-        );
-        
+        uint256 amountIn = _getAmountETHIn(token, amountOut);
         if (msg.value < amountIn) revert InsufficientETH();
-        
-        IWETH(WETH).deposit{value: amountIn}();
-        
-        if (!IWETH(WETH).transfer(pair, amountIn)) revert TransferFailed();
-        
-        if (isWETHToken0) {
-            IPancakeSwapV2Pair(pair).swap(0, amountOut, msg.sender, "");
-        } else {
-            IPancakeSwapV2Pair(pair).swap(amountOut, 0, msg.sender, "");
-        }
+
+        _swapETHForExactTokensToContract(token, amountOut, amountIn);
         
         uint256 refund = msg.value - amountIn;
         if (refund > 0) {
@@ -101,7 +53,7 @@ contract PancakeSwapBuyer {
         if (msg.value == 0) revert InsufficientETH();
         
         // calculate amountIn
-        uint256 ethForSwap = getAmountETHIn(token, amountOut);
+        uint256 ethForSwap = _getAmountETHIn(token, amountOut);
         if (msg.value <= ethForSwap) revert InsufficientETH();
         
         // swap ETH for tokens
@@ -162,7 +114,7 @@ contract PancakeSwapBuyer {
         }
     }
     
-    function getAmountETHIn(address token, uint amountOut) public view returns (uint amountIn) {
+    function _getAmountETHIn(address token, uint amountOut) internal view returns (uint amountIn) {
         if (token == address(0) || token == WETH) revert InvalidToken();
         
         address pair = IPancakeSwapV2Factory(PANCAKE_FACTORY).getPair(WETH, token);
